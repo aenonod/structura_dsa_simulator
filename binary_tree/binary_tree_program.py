d650676e3788ffebceb26f36bcf85983e4ef8624
Abegail E. Nonod <abegail.nonod13@gmail.com>
2025-12-29 02:22:34 +0800

    feat: ui to generate binary tree (#11)

--- binary_tree/binary_tree_program.py
+++ binary_tree/binary_tree_program.py
@@ -1,37 +1,38 @@
 from collections import deque
 
-class BinaryTree:
-    class Node:
-        def __init__(self, data):
-            self.data = data 
-            self.left = None
-            self.right = None
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-    def build_bt(self, data):
-        if not data:
-            return None
-        
-        if data[0].lower() == "n/a":
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def build_bt(self, height, values):
+        max_nodes = (2 ** height) - 1
+        values = values[:max_nodes]
+
+        if not values or values[0].lower() == "n/a":
             self.root = None
-            return None
-        
-        self.root = self.Node(data[0])
+            return
+
+        self.root = Node(values[0])
         queue = deque([self.root])
         i = 1
 
-        while queue and i < len(data):
+        while queue and i < len(values):
             current = queue.popleft()
 
-            if i < len(data):
-                if data[i].lower() != "n/a":
-                    current.left = self.Node(data[i])
-                    queue.append(current.left)
-                i += 1
+            # left child
+            if i < len(values) and values[i].lower() != "n/a":
+                current.left = Node(values[i])
+                queue.append(current.left)
+            i += 1
 
-            if i < len(data):
-                if data[i].lower() != "n/a":
-                    current.right = self.Node(data[i])
-                    queue.append(current.right)
-                i += 1
-
-        return self.root
+            # right child
+            if i < len(values) and values[i].lower() != "n/a":
+                current.right = Node(values[i])
+                queue.append(current.right)
+            i += 1
