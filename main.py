ed8868e227c20db474577671feefd0af699d3137
Abby <abegail.nonod13@gmail.com>
2026-01-01 15:15:18 +0800

    feat(main.py): setup main menu and add stack button

--- main.py
+++ main.py
@@ -31,7 +31,10 @@ class ProgramGUI(tk.Tk):
         self.canvas.itemconfig(self.bg_id, image=self.bg_img)
         self.canvas.coords(self.bg_id, 0, 0)
 
-        self.canvas.coords(self.play_btn_id, width * 0.50, height * 0.64)
+        if hasattr(self, "play_btn_id"):
+            self.canvas.coords(self.play_btn_id, width * 0.50, height * 0.64)
+        if hasattr(self, "stack_btn_id"):
+            self.canvas.coords(self.stack_btn_id, width * 0.50, height * 0.50)
 
     def force_redraw(self):
         w = self.canvas.winfo_width()
@@ -66,6 +69,31 @@ class ProgramGUI(tk.Tk):
         self.play_btn.pack(ipadx=50, ipady=50)
         self.play_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.play_btn)
 
+    def main_menu(self):
+        self.clear_page()
+        self.orig_bg = Image.open("assets/main_menu_page.png")
+        self.bg_img = None
+        self.bg_id = self.canvas.create_image(0, 0, anchor="nw")
+        self.create_stack_button()
+        self.force_redraw()
+
+    def create_stack_button(self):
+        self.stack_btn = tk.Button(self.canvas,
+            text="STACK",
+            font=("Press Start 2P", 18, "bold"),
+            fg="white",
+            bg="#6e7bb2",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=1,
+            width=20,
+            height=1, 
+            command=self.main_menu)
+        
+        self.stack_btn.pack(ipadx=50, ipady=50)
+        self.stack_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.stack_btn)
 
 if __name__ == "__main__":
     app = ProgramGUI()
