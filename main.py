333f9f86fa160ed0616ce6d339595a3c0ab253e4
Abegail E. Nonod <abegail.nonod13@gmail.com>
2026-01-01 15:59:57 +0800

    feat(main.py): setup main menu page (#23)
    * feat(main.py): setup main menu and add stack button
    
    * feat(main.py): add queue button
    
    * feat(main.py): add binary tree button
    
    * feat(main.py): add binary search tree button
    
    * feat(main.py): add recursion button
    
    * feat(main.py): add back button
    
    * feat(main.py): add meet the devs button

--- main.py
+++ main.py
@@ -31,7 +31,22 @@ class ProgramGUI(tk.Tk):
         self.canvas.itemconfig(self.bg_id, image=self.bg_img)
         self.canvas.coords(self.bg_id, 0, 0)
 
-        self.canvas.coords(self.play_btn_id, width * 0.50, height * 0.64)
+        if hasattr(self, "play_btn_id"):
+            self.canvas.coords(self.play_btn_id, width * 0.50, height * 0.64)
+        if hasattr(self, "stack_btn_id"):
+            self.canvas.coords(self.stack_btn_id, width * 0.50, height * 0.50)
+        if hasattr(self, "queue_btn_id"):
+            self.canvas.coords(self.queue_btn_id, width * 0.50, height * 0.58)
+        if hasattr(self, "bt_btn_id"):
+            self.canvas.coords(self.bt_btn_id, width * 0.50, height * 0.66)
+        if hasattr(self, "bst_btn_id"):
+            self.canvas.coords(self.bst_btn_id, width * 0.50, height * 0.74)
+        if hasattr(self, "recursion_btn_id"):
+            self.canvas.coords(self.recursion_btn_id, width * 0.50, height * 0.82)
+        if hasattr(self, "back_btn_id"):
+            self.canvas.coords(self.back_btn_id, width * 0.15, height * 0.92)
+        if hasattr(self, "devs_btn_id"):
+            self.canvas.coords(self.devs_btn_id, width * 0.85, height * 0.92)
 
     def force_redraw(self):
         w = self.canvas.winfo_width()
@@ -66,6 +81,145 @@ class ProgramGUI(tk.Tk):
         self.play_btn.pack(ipadx=50, ipady=50)
         self.play_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.play_btn)
 
+    def main_menu(self):
+        self.clear_page()
+        self.orig_bg = Image.open("assets/main_menu_page.png")
+        self.bg_img = None
+        self.bg_id = self.canvas.create_image(0, 0, anchor="nw")
+        self.create_stack_button()
+        self.create_queue_button()
+        self.create_bt_button()
+        self.create_bst_button()
+        self.create_recursion_button()
+        self.create_back_button()
+        self.create_devs_button()
+        self.force_redraw()
+
+    def create_stack_button(self):
+        self.stack_btn = tk.Button(self.canvas,
+            text="STACK",
+            font=("Press Start 2P", 18, "bold"),
+            fg="white",
+            bg="#6e7bb2",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=3.5,
+            width=20,
+            height=1, 
+            command=self.main_menu)
+        
+        self.stack_btn.pack(ipadx=50, ipady=50)
+        self.stack_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.stack_btn)
+
+    def create_queue_button(self):
+        self.queue_btn = tk.Button(self.canvas,
+            text="QUEUE",
+            font=("Press Start 2P", 18, "bold"),
+            fg="white",
+            bg="#6e7bb2",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=3.5,
+            width=20,
+            height=1, 
+            command=self.main_menu)
+        
+        self.queue_btn.pack(ipadx=50, ipady=50)
+        self.queue_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.queue_btn)
+
+    def create_bt_button(self):
+        self.bt_btn = tk.Button(self.canvas,
+            text="BINARY TREE",
+            font=("Press Start 2P", 18, "bold"),
+            fg="white",
+            bg="#6e7bb2",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=3.5,
+            width=20,
+            height=1, 
+            command=self.main_menu)
+        
+        self.bt_btn.pack(ipadx=50, ipady=50)
+        self.bt_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.bt_btn)
+
+    def create_bst_button(self):
+        self.bst_btn = tk.Button(self.canvas,
+            text="BINARY SEARCH TREE",
+            font=("Press Start 2P", 18, "bold"),
+            fg="white",
+            bg="#6e7bb2",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=3.5,
+            width=20,
+            height=1, 
+            command=self.main_menu)
+        
+        self.bst_btn.pack(ipadx=50, ipady=50)
+        self.bst_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.bst_btn)
+
+    def create_recursion_button(self):
+        self.recursion_btn = tk.Button(self.canvas,
+            text="RECURSION",
+            font=("Press Start 2P", 18, "bold"),
+            fg="white",
+            bg="#6e7bb2",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=3.5,
+            width=20,
+            height=1, 
+            command=self.main_menu)
+        
+        self.recursion_btn.pack(ipadx=50, ipady=50)
+        self.recursion_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.recursion_btn)
+
+    def create_back_button(self):
+        self.back_btn = tk.Button(self.canvas,
+            text="BACK",
+            font=("Press Start 2P", 15, "bold"),
+            fg="white",
+            bg="#17357a",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=3.5,
+            width=15,
+            height=1, 
+            command=self.welcome_page)
+        
+        self.back_btn.pack(ipadx=50, ipady=50)
+        self.back_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.back_btn)
+
+    def create_devs_button(self):
+        self.devs_btn = tk.Button(self.canvas,
+            text="MEET THE DEVS",
+            font=("Press Start 2P", 15, "bold"),
+            fg="white",
+            bg="#17357a",
+            activebackground="#1f4bb3",
+            relief="solid",
+            bd=6,
+            padx=10,
+            pady=3.5,
+            width=15,
+            height=1, 
+            command=self.welcome_page)
+        
+        self.devs_btn.pack(ipadx=50, ipady=50)
+        self.devs_btn_id = self.canvas.create_window(0, 0, anchor="center", window=self.devs_btn)
 
 if __name__ == "__main__":
     app = ProgramGUI()
