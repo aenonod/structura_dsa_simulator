b67310b163d24d09bf32627d230b01d50ab447fd
talamor-shella <shellatalamor@gmail.com>
2026-01-08 01:53:59 +0800

    refactor(binary_search_tree.py): change ui design same as bt

--- binary_search_tree/binary_search_tree_program.py
+++ binary_search_tree/binary_search_tree_program.py
@@ -1,17 +1,13 @@
-from tkinter import *
 import tkinter as tk
+from tkinter import *
 
-
-#7
 class Node:
     def __init__(self,value):
         self.value = value
         self.right = None
         self.left = None
 
-#1 GUI
-class BST(tk.Tk):
-
+class TreeGUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Structura")
@@ -19,32 +15,30 @@ class BST(tk.Tk):
         self.bind("<Escape>", lambda e: self.attributes("-fullscreen", False))
 
 
-        self.display_background()
-        self.display_get_user_input_frame()
-        #17
-        self.display_input_back_button()
-        #5
-        self.display_binary_search_tree()
-        self.binary_search_tree_frame() 
-        #17
-        self.display_bst_back_button()
-        
-        #5
-        self.bst_frame.place_forget()
+        self.canvas = tk.Canvas(self, highlightthickness=0)
+        self.canvas.pack(fill="both", expand=True)
 
-#8
+        self.bg_image = tk.PhotoImage(file="assets/background.png")
+        self.bg_id = self.canvas.create_image(0, 0, anchor="nw", image=self.bg_image, tags=("bg",))
+
+        self.setup_prog_label()
+        self.setup_values_input()
+        self.confirm_button()
+        self.back_button()
+        self.traversal_frame()
+
+    #for inserting new values in BST (left node= lower number or equal, right node = higher numbers)    
     def insert(self,root,value):
         if root is None:
             return Node(value)
         
-        if value <= root.value:
+        if value < root.value:
             root.left = self.insert(root.left,value)
         else:
             root.right = self.insert(root.right,value)
 
         return root
 
-#10
     def inorder(self,root,result):
         if root is None:
             return
@@ -52,52 +46,85 @@ class BST(tk.Tk):
         result.append(root.value)
         self.inorder(root.right,result)
 
-#12
     def draw_bst(self,node,x,y,dx):
         if not node:
             return
-        circle_radius = 25
-        v_space = 65
+        circle_radius = 15
+        v_space = 50
 
         if node.left:
-            next_dx_left = max(dx * 0.5, 30)
-            self.display_canvas_bst.create_line(x, y, x - dx, y + v_space, fill="white", width=2)
+            next_dx_left = max(dx * 0.5, 20)
+            self.canvas.create_line(x, y, x - dx, y + v_space, fill="white", width=2)
             self.draw_bst(node.left, x - dx, y + v_space, next_dx_left)
 
         if node.right:
-            next_dx_right = max(dx * 0.5, 30)
-            self.display_canvas_bst.create_line(x, y, x + dx, y + v_space, fill="white", width=2)
+            next_dx_right = max(dx * 0.5, 20)
+            self.canvas.create_line(x, y, x + dx, y + v_space, fill="white", width=2)
             self.draw_bst(node.right, x + dx, y + v_space, next_dx_right)
         
-        self.display_canvas_bst.create_oval(x-circle_radius, y-circle_radius, x+circle_radius, y+circle_radius, fill="#cab7d9")
-        self.display_canvas_bst.create_text(x, y, text=str(node.value), font=("Arial", 12, "bold"))
+        self.canvas.create_oval(x-circle_radius, y-circle_radius, x+circle_radius, y+circle_radius, fill="#cab7d9")
+        self.canvas.create_text(x, y, text=str(node.value), font=("Arial", 12, "bold"))
 
-#14
-    def display_count(self):
-        raw_count = self.entry_user.get()
-        valid_numbers = []
+    def setup_prog_label(self):
+        self.label_frame = Frame(self, bg = "#6e7bb2",
+                                 bd = 8, relief = "solid")
+        self.label_frame.place(relx=0.993, rely=0.01, width=450, height=60, anchor="ne")
 
-        for count in raw_count.split(","):
-            count = count.strip()
+        self.label_text = Label(self.label_frame, text="Binary Search Tree",
+                                font=("Press Start 2P", 15, "bold"),
+                                fg = "white",
+                                bg="#6e7bb2")
+        self.label_text.pack(padx=10, pady=10)
 
-            if count != "":
-                valid_numbers.append(int(count))
-        
-        total_count = len(valid_numbers)
+    def setup_values_input(self):
+        self.values_frame = tk.Frame(self, bg="#6e7bb2",
+                                     bd = 5, relief = "solid")
+        self.values_frame.place(relx=0.007, rely=0.09, width=1513, height=50, anchor="nw")
 
+        self.values_label = tk.Label(self.values_frame, text="Input number of values:",
+                                font=("Montserrat", 15, "bold"),
+                                fg="black",
+                                bg="#6e7bb2")
+        self.values_label.grid(row=0, column=0, padx=10, pady=7)
 
-        self.number_count_display.delete(0, END)
-        self.number_count_display.insert(0, total_count)
+        self.values_input = tk.Entry(self.values_frame,
+                                font=("Montserrat", 15), width=100,
+                                fg="#000000", bg="#FFFFFF")
+        self.values_input.grid(row=0, column=1, padx=0.9, sticky="e")
 
-#18
+        self.tip_label = tk.Label(self,
+                                text="", font=("Montserrat", 12),
+                                bg="black",
+                                fg="white")
+        self.tip_label.place(relx=0.993, rely=0.152, width=330, anchor="ne")
+        self.tip_label.config(text="Input must be minimum of 10, maximum of 30")
+
+        self.error_label = Label(self, text="", font=("Press Start 2P", 13), fg="red", bg="#6e7bb2")
+        self.error_label.place(relx=0.98, rely=0.109, anchor="ne")
+
+        #self.number_count_display = Entry(self, font = ("Press Start 2P", 13), relief = "flat", bg="black")
+        #self.number_count_display.place(relx= 0.886, rely= 0.195, width= 330, anchor = "center")
+
+    def confirm_button(self):
+        self.tree_btn = tk.Button(self, text="Generate Tree",
+                                font=("Montserrat", 15, "bold"), bd=8,
+                                fg="black",
+                                bg="#b3d9ff",
+                                padx=10, pady=7)
+        self.tree_btn.config(command=lambda: self.confirm_input())
+        self.tree_btn.place(relx=0.007, rely=0.01, width=180, height=60, anchor="nw")
+
     def back_button(self):
-         
-        self.bst_frame.place_forget()
-        self.input_frame.place(relx=0.5, rely=0.5, anchor="center")
-        self.binary_frame.place(relx=0.993, rely=0.01, width=450, height=60, anchor="ne")
-#2
+        self.back_btn = tk.Button(self, text="Back",
+                                font=("Press Start 2P", 15, "bold"), bd=8,
+                                fg="white",
+                                bg="#17357a",
+                                padx=10, pady=7)
+        self.back_btn.config(command=lambda: self.main_menu())
+        self.back_btn.place(relx=0.994, rely=0.99, width=150, height=50, anchor="se")
+
     def confirm_input(self):
-        raw_number_value = self.entry_user.get()
+        raw_number_value = self.values_input.get()
         number_values = []
 
         try:
@@ -117,105 +144,35 @@ class BST(tk.Tk):
         #9
             root = None
             for number in number_values:
-                root = self.insert(root,number)
-
-            #6
-            self.display_bg.itemconfigure(self.number_limit_text, state="hidden")
-            self.input_frame.place_forget()
-            self.binary_frame.place_forget()
-            self.bst_frame.place(x= 0,y= 0)
-
-            
-            self.draw_bst(root,780, 200, 360)
+                root = self.insert(root,number)   
+       
 
         except:
             self.error_label.config(text="INVALID")
             return
-
-    #11 (for checking only)
-        result = []
-        self.inorder(root,result)
-
-        print(number_values)
-        print("BST root value:", root.value)
-
-        print("Inorder Traversal(LTR):")
-        print(result)
             
-#1
-    def display_background(self):
-        self.display_bg = Canvas(self, width=1920, height=1080, highlightthickness=0)
-        self.display_bg.place(x=0, y=0)
 
-        self.bg_image = PhotoImage(file="assets/background.png")
-
-        self.display_bg.create_image(0, 0, image=self.bg_image, anchor="nw")
-
-#1
-    def display_get_user_input_frame(self):
-        self.input_frame = Frame(self, bg = "#a3afe0", width=1000, height=300, bd = 4, relief = "solid")
-        self.input_frame.place(relx=0.5, rely=0.5, anchor="center")
-
-        self.number_label = Label(self.input_frame,text="Enter any random number:",font=("Press Start 2P", 20, "bold"), fg = "black",bg="#a3afe0")
-        self.number_label.place(relx=0.5, rely=0.11, anchor="center")
-
-        self.entry_user = Entry(self.input_frame, font=("Press Start 2P",20), bd= 4, relief = "solid", justify = "left")
-        self.entry_user.place(relx=0.5, rely=0.35, anchor="center", width=900, height=60)
-        self.entry_user.bind("<Key>", lambda event: self.error_label.config(text=""))
-        self.entry_user.bind("<KeyRelease>", lambda e: self.display_count())
-    #1
-        self.button_confirm = Button(self.input_frame, text = "Confirm", font = ("Press Start 2P",20), fg = "white", bg = "#17357a", bd = 4, relief = "solid", command = self.confirm_input)
-        self.button_confirm.place(relx=0.5, rely=0.85, anchor="center", width=400, height=60)
-
-        self.number_limit_text = self.display_bg.create_text(765,int(1080 * 0.62), text="Input must be minimum of 10, maximum of 30",font=("Press Start 2P", 15, "bold"),fill="#fdacac")
-
-
-    
-
-#13 display the number count when inputting number
-        self.number_count = Label(self.input_frame, text = "Number count:", font = ("Press Start 2P", 20), fg = "black",bg = "#a3afe0")
-        self.number_count.place(relx= 0.223,rely= 0.55, anchor = "center")
-
-        self.number_count_display = Entry(self.input_frame, font = ("Press Start 2P", 20), relief = "flat", bg="#a3afe0")
-        self.number_count_display.place(relx= 0.49, rely= 0.55, width= 156, height= 52, anchor = "center")
+        result = []
+        self.inorder(root, result)
+        self.traversal_label.config(text="Inorder: " + " ".join(map(str, result)))
         
-#3
-        self.error_label = Label(self.input_frame, text="", font=("Press Start 2P", 20), fg="red", bg="#a3afe0")
-        self.error_label.place(relx=0.86, rely=0.55, anchor="center")
+        self.canvas.delete("all")
+        self.canvas.create_image(0, 0, anchor="nw", image=self.bg_image)
 
-#1 
-    def binary_search_tree_frame(self):
-        self.binary_frame = Frame(self, bg = "#6e7bb2", bd = 8, relief = "solid")
-        self.binary_frame.place(relx=0.993, rely=0.01, width=450, height=60, anchor="ne")
+        self.draw_bst(root,780,170,400)
 
-        self.label_binary_text = Label(self.binary_frame,text="Binary Search Tree",font=("Press Start 2P", 15, "bold"), fg = "white",bg="#6e7bb2")
-        self.label_binary_text.pack(padx=10, pady=10)
+    def traversal_frame(self):
+        self.traversal_container = tk.Frame(self, bg="#b3d9ff",
+                                bd = 5, relief = "solid")
+        self.traversal_container.place(relx=0.007, rely=0.99, width=1350, height=50, anchor="sw")
 
-    #16
-    def display_input_back_button(self):
-        self.input_back_button = Button(self, text = "Back",font = ("Press Start 2P", 15, "bold"), fg = "white", bg ="#17357a", bd = 8,padx=10, pady=7)
-        self.input_back_button.place(relx=0.994, rely=0.99, width=150, height=50, anchor="se")
-
-    def display_bst_back_button(self):
-        self.bst_back_button = Button(self, text = "Back",font = ("Press Start 2P", 15, "bold"), fg = "white", bg ="#17357a", bd = 8,padx=10, pady=7, command = self.back_button)
-        self.bst_back_button.place(relx=0.994, rely=0.99, width=150, height=50, anchor="se")
-#4
-    def display_binary_search_tree(self):
-        self.bst_frame = Frame(self, width=1920, height=1080)
-        self.bst_frame.place(x=0, y=0)
-
-        self.display_canvas_bst = Canvas(self.bst_frame, width=1920, height=1080, highlightthickness=0)
-        self.display_canvas_bst.place(x= 0, y= 0)
-        
-        self.display_canvas_bst.create_image(0, 0, image=self.bg_image, anchor="nw")
-
-        self.bst_title_frame = Frame(self.display_canvas_bst, bg = "#6e7bb2", width=450, height=60, bd = 8, relief = "solid")
-        
-        self.bst_title = Label(self.bst_title_frame,text="Binary Search Tree",font=("Press Start 2P", 15, "bold"), fg = "white",bg="#6e7bb2")
-        self.bst_title.pack(padx=25, pady=9)
-
-        self.display_canvas_bst.create_window(1075, 10,anchor="nw",window = self.bst_title_frame)
+        self.traversal_label = tk.Label(self.traversal_container, text="Inorder:",
+                                font=("Montserrat", 15, "bold"),
+                                fg="black",
+                                bg="#b3d9ff",
+                                justify="left", anchor="w")
+        self.traversal_label.pack(padx=2, pady=2, fill="both")
 
 if __name__ == "__main__":
-    app = BST()
+    app = TreeGUI()
     app.mainloop()
