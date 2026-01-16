bd9acc7f1eb507910c8d9357f55abd43f0ab7b41
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-01-17 04:08:48 +0800

    style(ui): modify the values into 80% for the stack

--- stack/car_parking_lot.py
+++ stack/car_parking_lot.py
@@ -7,34 +7,34 @@ class Car:
         self.car = car
         self.canvas = canvas
         self.x = x
-        self.y = -60
-        self.speed = 10
+        self.y = -48
+        self.speed = 8
         self.stop_y = stop_y
         self.image = self.create_car_image()
         self.tk_image = ImageTk.PhotoImage(self.image)
         self.id = self.canvas.create_image(self.x, self.y, image=self.tk_image)
 
     def create_car_image(self):
-        image = Image.new("RGBA", (60, 100), (0, 0, 0, 0))
+        image = Image.new("RGBA", (48, 80), (0, 0, 0, 0))
         draw = ImageDraw.Draw(image)
 
-        draw.rectangle([10, 0, 50, 90], fill="#17357A")
+        draw.rectangle([8, 0, 40, 72], fill="#17357A")
 
-        draw.rectangle([15, 70, 45, 80], fill="#000000")
-        draw.rectangle([15, 10, 45, 20], fill="#000000")
+        draw.rectangle([12, 56, 36, 64], fill="#000000")
+        draw.rectangle([12, 8, 36, 16], fill="#000000")
 
-        draw.rectangle([12.5, 87.5, 17.5, 90], fill="#ffde59")
-        draw.rectangle([42.5, 87.5, 47.5, 90], fill="#ffde59")
-        draw.rectangle([12.5, 0, 17.5, 2.5], fill="#ffde59")
-        draw.rectangle([42.5, 0, 47.5, 2.5], fill="#ffde59")
+        draw.rectangle([10, 70, 14, 72], fill="#ffde59")
+        draw.rectangle([34, 70, 38, 72], fill="#ffde59")
+        draw.rectangle([10, 0, 14, 2], fill="#ffde59")
+        draw.rectangle([34, 0, 38, 2], fill="#ffde59")
 
         try:
             font_path = "assets/PressStart2P-vaV7.ttf"
-            font = ImageFont.truetype(font_path, 20)
+            font = ImageFont.truetype(font_path, 16)
         except OSError:
             font = ImageFont.load_default()
 
-        draw.text((20, 45), self.car, font=font, fill="#ffffff")
+        draw.text((16, 36), self.car, font=font, fill="#ffffff")
         return image
 
     def move_in(self, callback=None):
@@ -52,7 +52,7 @@ class ParkingLot:
 
         self.root = root
         self.root.title("Structura: Parking Lot")
-        self.root.geometry("1920x1080")
+        self.root.geometry("1536x864")
 
         try:
             self.font = "Press Start 2P"
@@ -82,37 +82,37 @@ class ParkingLot:
         except:
             self.root.configure(bg="#808080")
 
-        information_frame = tk.Frame(self.root, bg="#000000", bd=10, relief="solid")
-        information_frame.place(anchor="center", x=300, y=75, width=400, height=100)
-        tk.Label(information_frame, text="Parking Lot", font=(self.font, 20), fg="#ffffff", bg="#000000").place(anchor="center", relx=0.5, rely=0.25)
-        self.status_label = tk.Label(information_frame, text="Ready", fg="#6e7bb2", bg="#000000", font=(self.font, 10))
+        information_frame = tk.Frame(self.root, bg="#000000", bd=8, relief="solid")
+        information_frame.place(anchor="center", x=240, y=60, width=320, height=80)
+        tk.Label(information_frame, text="Parking Lot", font=(self.font, 16), fg="#ffffff", bg="#000000").place(anchor="center", relx=0.5, rely=0.25)
+        self.status_label = tk.Label(information_frame, text="Ready", fg="#6e7bb2", bg="#000000", font=(self.font, 8))
         self.status_label.place(anchor="center", relx=0.5, rely=0.75)
 
     def setup_parking_lot(self):
 
-        self.parking_lot = tk.Canvas(self.root, width=400, height=810, bg="#6e7bb2", bd=10, relief="solid")
-        self.parking_lot.place(anchor="center", x=300, y=550)
+        self.parking_lot = tk.Canvas(self.root, width=320, height=648, bg="#6e7bb2", bd=8, relief="solid")
+        self.parking_lot.place(anchor="center", x=240, y=440)
 
     def setup_ui(self, title, park, unpark):
 
-        title_frame = tk.Frame(self.root, bg="#6e7bb2", bd=10, relief="solid")
-        title_frame.place(anchor="center", x=1620, y=63, width=540, height=63)
+        title_frame = tk.Frame(self.root, bg="#6e7bb2", bd=8, relief="solid")
+        title_frame.place(anchor="center", x=1296, y=50.4, width=432, height=50.4)
 
-        tk.Label(title_frame, text=title, font=(self.font, 20), bg="#6e7bb2", fg="#ffffff").place(anchor="center", relx=0.5, rely=0.5)
+        tk.Label(title_frame, text=title, font=(self.font, 16), bg="#6e7bb2", fg="#ffffff").place(anchor="center", relx=0.5, rely=0.5)
 
-        control_frame = tk.Frame(self.root, bg="#6e7bb2", bd=10, relief="solid")
-        control_frame.place(anchor="center", x=960, y=790, width=600, height=300)
+        control_frame = tk.Frame(self.root, bg="#6e7bb2", bd=8, relief="solid")
+        control_frame.place(anchor="center", x=768, y=632, width=480, height=240)
 
-        tk.Label(control_frame, text="Enter Car:", font=(self.font, 20), bg="#6e7bb2", fg="#000000").place(anchor="center", relx=0.5, y=30)
+        tk.Label(control_frame, text="Enter Car:", font=(self.font, 16), bg="#6e7bb2", fg="#000000").place(anchor="center", relx=0.5, y=24)
 
         vcmd = (self.root.register(self.validate_input), "%P")
-        self.text_box = tk.Entry(control_frame, validate="key", validatecommand=vcmd, font=(self.font, 20), width=1, bd=10, relief="solid")
-        self.text_box.place(anchor="center", relx=0.5, y=90)
+        self.text_box = tk.Entry(control_frame, validate="key", validatecommand=vcmd, font=(self.font, 16), width=1, bd=8, relief="solid")
+        self.text_box.place(anchor="center", relx=0.5, y=72)
 
-        tk.Button(self.root, text="Back", bg="#17357A", fg="#ffffff", font=(self.font, 20), bd=10, relief="solid",command=lambda: None).place(anchor="center", x=1800, y=963, width=180, height=63)
-        tk.Button(control_frame, text="Park", bg="#17357A", fg="#ffffff", font=(self.font, 20), bd=10, relief="solid", command=park).place(anchor="center", relx=0.5, y=155, relwidth=0.4, height=63)
-        tk.Button(control_frame, text="Unpark", bg="#17357A", fg="#ffffff", font=(self.font, 20), bd=10, relief="solid", command=unpark).place(anchor="center", relx=0.5, y=230, relwidth=0.4, height=63)
-        tk.Button(self.root, text="Reset", bg="#17357A", fg="#ffffff", font=(self.font, 20), bd=10, relief="solid", command=self.reset).place(anchor="center", x=960, y=390, width=180, height=63)
+        tk.Button(self.root, text="Back", bg="#17357A", fg="#ffffff", font=(self.font, 16), bd=8, relief="solid",command=lambda: None).place(anchor="center", x=1440, y=770.4, width=144, height=50.4)
+        tk.Button(control_frame, text="Park", bg="#17357A", fg="#ffffff", font=(self.font, 16), bd=8, relief="solid", command=park).place(anchor="center", relx=0.5, y=124, relwidth=0.4, height=50.4)
+        tk.Button(control_frame, text="Unpark", bg="#17357A", fg="#ffffff", font=(self.font, 16), bd=8, relief="solid", command=unpark).place(anchor="center", relx=0.5, y=184, relwidth=0.4, height=50.4)
+        tk.Button(self.root, text="Reset", bg="#17357A", fg="#ffffff", font=(self.font, 16), bd=8, relief="solid", command=self.reset).place(anchor="center", x=768, y=312, width=144, height=50.4)
 
     def validate_input(self, P):
         if len(P) <= 1:
@@ -149,12 +149,12 @@ class ParkingLot:
 
     def setup_table(self):
 
-        border_frame = tk.Frame(self.root, bd=10, relief="solid")
-        border_frame.place(anchor="center", x=960, y=270, width=653, height=154)
+        border_frame = tk.Frame(self.root, bd=8, relief="solid")
+        border_frame.place(anchor="center", x=768, y=216, width=547, height=126)
 
         style = ttk.Style()
-        style.configure("Treeview", font=(self.font, 20), rowheight=self.font_metrics(self.font, 20, "linespace"))
-        style.configure("Treeview.Heading", font=(self.font, 10))
+        style.configure("Treeview", font=(self.font, 16), rowheight=self.font_metrics(self.font, 16, "linespace"))
+        style.configure("Treeview.Heading", font=(self.font, 8))
 
         columns = ("Car", "Number of Arrivals", "Number of Departures")
         self.tree = ttk.Treeview(border_frame, columns=columns, show="headings", height=4)
@@ -162,7 +162,7 @@ class ParkingLot:
         for column in columns:
 
             self.tree.heading(column, text=column)
-            self.tree.column(column, width=self.font_measure(self.font, 10, column) + 40, anchor="center")
+            self.tree.column(column, width=self.font_measure(self.font, 8, column) + 32, anchor="center")
 
         self.tree.place(anchor="center", relx=0.5, rely=0.5, relwidth=1, relheight=1)
 
