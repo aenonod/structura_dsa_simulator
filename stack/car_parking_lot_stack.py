863a9a3d496ae61c0cfe8ea8c9a6386097d7f646
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-01-19 16:54:16 +0800

    feat(ui): add error for the stack

--- stack/car_parking_lot_stack.py
+++ stack/car_parking_lot_stack.py
@@ -108,6 +108,9 @@ class StackParkingLot(tk.Frame, ParkingLot):
 
         car = self.text_box.get().upper()
         if not car: return
+        if len(self.stack) == 0:
+            messagebox.showerror("Error", "Parking Lot is empty!")
+            return
         if car not in self.stack:
             messagebox.showerror("Error", "Car not parked yet!")
             return
