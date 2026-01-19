0156e78afab4aa45a0b45b5c06e7adaa41adf7b4
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-01-19 16:49:51 +0800

    fix(ui): correct error text for the queue

--- queue/car_parking_lot_queue.py
+++ queue/car_parking_lot_queue.py
@@ -60,7 +60,7 @@ class QueueParkingLot(tk.Frame, ParkingLot):
         car = self.text_box.get().upper()
         if not car: return
         if len(self.queue) == 4:
-            messagebox.showerror("Error", "Parking Garage is full!")
+            messagebox.showerror("Error", "Parking Lot is full!")
             return
         if car in self.queue:
             messagebox.showerror("Error", "Car already parked!")
