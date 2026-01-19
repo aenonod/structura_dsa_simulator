078a0a96b06dad061c647be95b72f1d32a6a0053
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-01-19 15:41:42 +0800

    refactor(ui): modify warning into error for the queue

--- queue/car_parking_lot_queue.py
+++ queue/car_parking_lot_queue.py
@@ -58,10 +58,10 @@ class QueueParkingLot(ParkingLot):
         car = self.text_box.get().upper()
         if not car: return
         if len(self.queue) == 4:
-            messagebox.showwarning("Error", "Parking Garage is full!")
+            messagebox.showerror("Error", "Parking Garage is full!")
             return
         if car in self.queue:
-            messagebox.showwarning("Error", "Car already parked!")
+            messagebox.showerror("Error", "Car already parked!")
             return
 
         if car not in self.cars:
